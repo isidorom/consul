a9213959b2f403f4aa4a8f3706a20d396652e057
isidorom <42376543+isidorom@users.noreply.github.com>
2022-12-19 09:20:50 +0100

    Update sms_api.rb

--- lib/sms_api.rb
+++ lib/sms_api.rb
@@ -1,39 +1,54 @@
-require "open-uri"
+#require "open-uri"
+require "twilio-ruby"
 class SMSApi
   attr_accessor :client
 
+  #def initialize
+  #  @client = Savon.client(wsdl: url)
+  #end
+
+  #def url
+  #  return "" unless end_point_available?
+  SID = Rails.application.secrets.sms_account_sid
+  AUTH_TOKEN = Rails.application.secrets.sms_auth_token
+  PHONE_NUMBER = Rails.application.secrets.sms_phone_number
+
+  #  URI.parse(Rails.application.secrets.sms_end_point).to_s
+  #end
+
+  #def authorization
+  #  Base64.encode64("#{Rails.application.secrets.sms_username}:#{Rails.application.secrets.sms_password}")
   def initialize
-    @client = Savon.client(wsdl: url)
-  end
-
-  def url
-    return "" unless end_point_available?
-
-    URI.parse(Rails.application.secrets.sms_end_point).to_s
-  end
-
-  def authorization
-    Base64.encode64("#{Rails.application.secrets.sms_username}:#{Rails.application.secrets.sms_password}")
+        @client = Twilio::REST::Client.new SID, AUTH_TOKEN
   end
 
   def sms_deliver(phone, code)
     return stubbed_response unless end_point_available?
 
-    response = client.call(:enviar_sms_simples, message: request(phone, code))
-    success?(response)
+    #response = client.call(:enviar_sms_simples, message: request(phone, code))
+    #success?(response)
+  #end
+    message = @client.messages.create(
+        body: "Este es el código de verificación de su teléfono: #{code}",
+        from: PHONE_NUMBER,
+        to: phone)
+    
+  #def request(phone, code)
+  #  { autorizacion:  authorization,
+  #    destinatarios: { destinatario: phone },
+  #    texto_mensaje: "Clave para verificarte: #{code}. Gobierno Abierto",
+  #    solicita_notificacion: "All" }
+   success?(message) 
   end
 
-  def request(phone, code)
-    { autorizacion:  authorization,
-      destinatarios: { destinatario: phone },
-      texto_mensaje: "Clave para verificarte: #{code}. Gobierno Abierto",
-      solicita_notificacion: "All" }
+  #def success?(response)
+  #  response.body[:respuesta_sms][:respuesta_servicio_externo][:texto_respuesta] == "Success"
+  #end
+  
+  def success?(message)
+    message.error_code.blank?
   end
-
-  def success?(response)
-    response.body[:respuesta_sms][:respuesta_servicio_externo][:texto_respuesta] == "Success"
-  end
-
+  
   def end_point_available?
     Rails.env.staging? || Rails.env.preproduction? || Rails.env.production?
   end
